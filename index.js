e8cdb1188793670fcbdcc72d40d87cdd679e6237
KagisoRabeng <kagisorabeng3@gmail.com>
2024-09-09 09:55:58 +0200

    added a new page

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const example = document.getElementById("eg")
+ const form = document.getElementById("myForm");
+ const formMessage = document.getElementById("formMessage");
 
-button.addEventListener('click', () => {
-   const message = document.createElement('p');
-    message.textContent = "Boop!"
-    example.appendChild(message)
-}) 
+ form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    if(!email || !name) {
+        formMessage.textContent = "Fill in the missing part(s)";
+        return;
+    };
+
+    console.log(`Name: ${name}, Email: ${email}`)
+    formMessage.textContent = "Form Submitted Successfully!";
+
+    window.location.href = './profile.html';
+ });
+
